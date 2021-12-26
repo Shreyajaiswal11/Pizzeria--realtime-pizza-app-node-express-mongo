e250ef4305ae98a05fa855a15aff74a16205b6f5
Shreya <shreyajaiswal1102@gmail.com>
2021-12-26 23:53:41 +0530

    addto cart

--- server.js
+++ server.js
@@ -1,13 +1,53 @@
+require('dotenv').config()
 const express=require("express");
 const app= express();
 const ejs=require("ejs");
 const Path=require("path");
 const expressLayout =require("express-ejs-layouts");
-const res = require("express/lib/response");
-const Port=process.env.Port || 5000
+const Port=process.env.Port || 5000;
+const mongoose=require('mongoose')
+const session =require('express-session')
+const flash=require('express-flash')
+const MongoDbStore=require('connect-mongo')
 
+// //database connection
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/pizza';
+mongoose.connect(dbUrl, {
+    useNewUrlParser:true,
+    /*useCreateIndex:true,*/
+    useUnifiedTopology:true
+});
+
+mongoose.connect(dbUrl);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console,"connection error:"));
+db.once("open",()=> {
+    console.log("Database connected");
+});
+
+// session config
+app.use(session({
+    secret:process.env.COOKIE_SECRET,
+    resave: false,
+    store: MongoDbStore.create({
+        // mongoUrl:'mongodb://localhost:27017/pizza'
+        client:db.getClient()
+    }),
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
+    
+}))
+
+app.use(flash())
 // Assets
 app.use(express.static('public'))
+app.use(express.json())
+
+//Global middleware
+app.use((req,res,next) =>{
+    res.locals.session=req.session
+    next()
+})
 
 //set the template engine
 app.use(expressLayout)
@@ -17,16 +57,6 @@ app.set('view engine','ejs')
 app.listen(Port, () =>{
     console.log(`listinning to port ${Port}`)
 })
-app.get("/",(req,res) =>{
-    res.render("home")
-}) 
-app.get("/cart",(req,res) =>{
-    res.render('customers/cart')
-})
-app.get('/login',(req,res) =>{
-    res.render('auth/login')
-})
-app.get('/register',(req,res) =>{
-    res.render('auth/register')
-})
+require('./routes/web')(app)
+
 
